f46b8da8a7af6a13abac3d7a170d0ae6aa133657
Dinesh_kumar <139780475+Dinesh-2103@users.noreply.github.com>
2023-08-13 17:32:07 +0530

    Add files via upload

---
+++ Utils/Endpoints.java
@@ -0,0 +1,16 @@
+package com.example.blood_copy.Utils;
+
+public class Endpoints {
+
+  private static final String base_url="https://non-euclidean-oaks.000webhostapp.com/";
+  public static final String register_url= base_url+"register.php";
+  public static final String login_url= base_url+"login.php";
+  public static final String upload_request= base_url+"upload_requests.php";
+  public static final String requests= base_url+"requests.php";
+  public static final String search_donors= base_url+"search_donors.php";
+  public static final String donor= base_url+"donor.php";
+  public static final String profile= base_url+"profile.php";
+  public static final String your_content= base_url+"your_cnt.php";
+
+
+}
